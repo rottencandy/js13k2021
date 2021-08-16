81c70a9896adfd5c24a489b71e402acb1e9e3843
Mohammed Saud <md.saud020@gmail.com>
2021-08-17 01:49:21 +0530

    notify on watch mode errors

--- scripts/dev.js
+++ scripts/dev.js
@@ -2,10 +2,15 @@ const glslxPlugin = require('esbuild-plugin-glslx');
 require('esbuild').build({
   entryPoints: ['src/main.js', 'src/app.css'],
   bundle: true,
-  watch: true,
   charset: 'utf8',
   target: 'es6',
   format: 'iife',
   outdir: 'app',
+  watch: {
+    onRebuild(error, result) {
+      if (error) console.error('watch build failed:', error.errors)
+      else console.log('watch build succeeded:', result)
+    },
+  },
   plugins: [glslxPlugin()],
 }).catch(() => process.exit(1))
