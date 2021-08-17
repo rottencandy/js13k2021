fe27528e5d9e8d81c71f610037a4a3af9c967d03
Mohammed Saud <md.saud020@gmail.com>
2021-08-17 10:20:14 +0530

    improve build script

--- bundle.sh
+++ bundle.sh
@@ -6,5 +6,8 @@ rm zip/*
 echo Creating new zip...
 zip -j -9 zip/game app/*
 echo Finished.
-echo Zip size: `stat --format="%s bytes" zip/game.zip`
-echo 13Kb = 13,312 bytes
+
+USED=$(stat --format="%s" zip/game.zip)
+echo "Zip size:  $USED bytes"
+echo "Limit:     13,312 bytes"
+echo "Remaining: $((13312 - $USED))"
