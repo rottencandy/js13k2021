69e051bf57611cce6a00831d0e70a907436f2960
Mohammed Saud <md.saud020@gmail.com>
2021-09-01 11:23:47 +0530

    display note for potrait devices

--- src/init.js
+++ src/init.js
@@ -1,11 +1,13 @@
 import { GAME_WIDTH, GAME_HEIGHT, CANVAS, CANVAS2D, WIDTH, HEIGHT, STYLE } from './globals.js';
-import { MIN } from './util.js';
+import { MIN, getById } from './util.js';
 import { setupKeyListener } from './engine/input.js';
 
 onresize = () => {
   const ratio = MIN(innerWidth / GAME_WIDTH, innerHeight / GAME_HEIGHT);
   CANVAS[STYLE][WIDTH] = CANVAS2D[STYLE][WIDTH] = GAME_WIDTH * ratio + 'px';
   CANVAS[STYLE][HEIGHT] = CANVAS2D[STYLE][HEIGHT] = GAME_HEIGHT * ratio + 'px';
+  // display note if device is in potrait
+  getById('d').style.display = innerWidth < innerHeight ? 'block':'none';
 };
 
 CANVAS[WIDTH] = CANVAS2D[WIDTH] = GAME_WIDTH;
